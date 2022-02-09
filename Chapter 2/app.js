7a4eea8bc4886878ddccef25a32382f7074b87fe
Zachary Boelter <zboelter@gmail.com>
2022-02-09 14:07:25 -0600

    fixed loop

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -1,4 +1,10 @@
 // “Write a loop that makes seven calls to console.log to output the following triangle:”
 
-// for (var result = '#'; result.length<=7; result += '#');
-// console.log(result);
+for (var result = '#'; result.length<=7; result += '#'){
+    console.log(result);
+}
+
+
+// fizzbuzz
+
+// for ()
