a4ebe99b837abf38f49e9bdf93ea641d69eee5ac
Zachary Boelter <zboelter@gmail.com>
2022-02-09 14:21:51 -0600

    chessboard

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -7,17 +7,33 @@
 
 // fizzbuzz
 
-for (let answer = 1; answer <= 100; answer++) {
-    if (answer % 3 === 0 && answer % 5 === 0) {
-        console.log('FizzBuzz');
-    }
-   else if (answer % 3 === 0) {
-        console.log('Fizz');
-    } 
-    else if (answer % 5 === 0) {
-        console.log('Buzz');
-    } 
-    else {
-        console.log(answer);
-    }
+// for (let answer = 1; answer <= 100; answer++) {
+//     if (answer % 3 === 0 && answer % 5 === 0) {
+//         console.log('FizzBuzz');
+//     }
+//    else if (answer % 3 === 0) {
+//         console.log('Fizz');
+//     } 
+//     else if (answer % 5 === 0) {
+//         console.log('Buzz');
+//     } 
+//     else {
+//         console.log(answer);
+//     }
+// }
+
+var size = 8; //this is the variable setting
+
+var board = "";//this is the empty string we're going to add either ' ' , '#' or newline
+
+for (var y = 0; y < size; y++) {   /*in the outer loop we add newline to seperate rows*/
+  for (var x = 0; x < size; x++) {/*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
+    if ((x + y) % 2 == 0)
+      board += " ";
+    else
+      board += "#";
+  }
+  board += "\n";
 }
+
+console.log(board);
