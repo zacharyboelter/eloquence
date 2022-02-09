ba267d3de8278220598cc168de1482bb1e3dac9e
Zachary Boelter <zboelter@gmail.com>
2022-02-09 16:26:26 -0600

    recursive and custom math.min

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -22,18 +22,44 @@
 //     }
 // }
 
-var size = 8; //this is the variable setting
+// var size = 8; //this is the variable setting
 
-var board = "";//this is the empty string we're going to add either ' ' , '#' or newline
+// var board = "";//this is the empty string we're going to add either ' ' , '#' or newline
 
-for (var y = 0; y < size; y++) {   /*in the outer loop we add newline to seperate rows*/
-  for (var x = 0; x < size; x++) {/*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
-    if ((x + y) % 2 == 0)
-      board += " ";
-    else
-      board += "#";
+// for (let y = 0; y < size; y++) {   /*in the outer loop we add newline to seperate rows*/
+//     for (let x = 0; x < size; x++) {/*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
+//         if ((x + y) % 2 == 0)
+//             board += " ";
+//         else
+//             board += "#";
+//     }
+//     board += "\n";
+// }
+
+// console.log(board);
+
+
+// RECURSIVE FUNCTIONS
+
+// function findSolution(target) {
+//     function find(current, history) {
+//         if (current == target) {
+//             return history;
+//         } else if (current > target) {
+//             return null;
+//         } else {
+//             return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
+//         }
+//     }
+//     return find(1, "1");
+// }
+
+// Math.min
+
+function min(x, y) {
+    if (x < y){
+      return x;
+    } else{
+      return y;
+    }
   }
-  board += "\n";
-}
-
-console.log(board);
