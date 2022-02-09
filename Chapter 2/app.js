c639d54297bea6a87ff95654b598bd5eaa6e97a6
Zachary Boelter <zboelter@gmail.com>
2022-02-09 14:16:53 -0600

    fizzybuzzy

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -1,10 +1,23 @@
 // “Write a loop that makes seven calls to console.log to output the following triangle:”
 
-for (var result = '#'; result.length<=7; result += '#'){
-    console.log(result);
-}
+// for (var result = '#'; result.length<=7; result += '#'){
+//     console.log(result);
+// }
 
 
 // fizzbuzz
 
-// for ()
+for (let answer = 1; answer <= 100; answer++) {
+    if (answer % 3 === 0 && answer % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+   else if (answer % 3 === 0) {
+        console.log('Fizz');
+    } 
+    else if (answer % 5 === 0) {
+        console.log('Buzz');
+    } 
+    else {
+        console.log(answer);
+    }
+}
