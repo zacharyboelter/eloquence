9a606367d433586427ece3a091820b6ae7d85f72
Zachary Boelter <zboelter@gmail.com>
2022-02-10 11:53:55 -0600

    counting beans

--- Chapter 2/app.js
+++ Chapter 2/app.js
@@ -93,3 +93,14 @@ function isEven(n) {
         return isEven(n);
     }
 }
+
+// BEAN COUNTING
+
+function countBs(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i += 1){
+        if (str.charAt(i) === 'B')
+            count += 1;
+    }
+    return count;
+}
